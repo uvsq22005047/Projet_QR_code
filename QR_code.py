bd21037203527c06fb4e2d0622693e29085d7a59
Clémence GERMETTE <clemence.germette@ens.uvsq.fr>
2022-05-06 22:56:56 +0200

    fonction de vérification de la rotation du QR code

--- QR_code.py
+++ QR_code.py
@@ -6,19 +6,25 @@ from tkinter import filedialog
 import PIL as pil
 from PIL import Image
 from PIL import ImageTk 
+import numpy
 
 # Constantes
 
 # Variables globals
-
-
+fichier = None
 
 # Fonctions
 
 def nbrCol(matrice):
+    """
+    renvoi le nombre de colonne d'une matrice
+    """
     return len(matrice[0])
 
 def nbrLig(matrice):
+    """
+    renvoie le nombre de ligne d'une matrice
+    """
     return len(matrice)
 
 def saving(matPix, filename):#sauvegarde l'image contenue dans matpix dans le fichier filename
@@ -38,18 +44,106 @@ def loading(filename):#charge le fichier image filename et renvoie une matrice d
             mat[i][j]= 0 if toLoad.getpixel((j,i)) == 0 else 1
     return mat
 
-def charger():
-    global filename, photo, picture_QR_code
-    filename = filedialog.askopenfile(mode='rb', title='Choose a file')
+def charger(filename):
+    global fichier, photo, picture_QR_code
+    fichier = filename
     img = pil.Image.open(filename)
     photo = ImageTk.PhotoImage(img)  
     picture_QR_code.config(width = img.size[0], height = img.size[1])
     dessin = picture_QR_code.create_image(2,2,anchor = 'nw', image=photo)
-    
 
-def decode(filename):
-    mat = loading(filename)
-    print(mat)
+def selection_fichier():
+    """
+    Affiche sur l'interface graphique le QR code à décoder
+    """
+    
+    filename = filedialog.askopenfile(mode='rb', title='Choose a file')
+    charger(filename)
+        
+def rotate_droite(matrice):
+
+    mat_rot=[[0]*nbrLig(matrice) for i in range(nbrCol(matrice))]
+
+    for i in range(nbrLig(mat_rot)):
+        for j in range(nbrCol(mat_rot)):
+            mat_rot[i][j] = matrice[nbrLig(matrice)-1-j][i]
+    
+    return mat_rot
+
+def rotate_gauche(matrice):
+   
+    mat_rot=[[0]*nbrLig(matrice) for i in range(nbrCol(matrice))]
+    
+    for i in range(nbrLig(mat_rot)):
+        for j in range(nbrCol(mat_rot)):
+            mat_rot[i][j] = matrice[j][nbrLig(matrice)-1-i]
+    
+    return mat_rot
+
+def extraire_matrice(g_mat, p_mat):
+    mat =[[0]*nbrLig(p_mat) for i in range(nbrCol(p_mat))]
+    for i in range(nbrLig(p_mat)):
+            for j in range(nbrCol(p_mat)):
+                mat[i][j] = g_mat[i][j]
+    return mat
+
+def verif_sens_QR_code(mat):
+    global coin_nw, coin_ne, coin_se, coin_sw
+
+    mat_coin = loading("coin.png")
+    coin_nw, coin_ne, coin_se, coin_sw = False, False, False, False
+
+    mat2 = extraire_matrice(mat, mat_coin)
+
+    if mat2 == mat_coin:
+        coin_nw = True
+        
+    mat = rotate_gauche(mat)
+    mat2 = extraire_matrice(mat, mat_coin)
+
+    if mat2 == mat_coin:
+        coin_ne = True
+        
+    mat = rotate_gauche(mat)
+    mat2 = extraire_matrice(mat, mat_coin)
+
+    if mat2 == mat_coin:
+        coin_se = True
+        
+    mat = rotate_gauche(mat)
+    mat2 = extraire_matrice(mat, mat_coin)
+        
+    if mat2 == mat_coin:
+        coin_sw = True
+
+    print(coin_nw, coin_ne, coin_se, coin_sw)
+
+def decode():
+    if fichier == None:
+        None
+    else:
+        mat = loading(fichier)
+        verif_sens_QR_code(mat)
+
+        if coin_nw == True and coin_ne == False and coin_se == True and coin_sw == True:
+            mat = rotate_droite(mat)
+        
+        elif coin_nw == True and coin_ne == True and coin_se == True and coin_sw == False:
+            mat = rotate_gauche(mat)
+
+        elif coin_nw == False and coin_ne == True and coin_se == True and coin_sw == True:
+            mat = rotate_gauche(rotate_gauche(mat))
+        
+        if coin_nw == True and coin_ne == True and coin_se == False and coin_sw == True:
+            None
+        else:
+            saving(mat,"new.png")
+            charger("new.png")
+
+
+        
+        
+        
 
 
 # Affichage graphique
@@ -59,8 +153,8 @@ main_windows.title("Lecteur de QR Code")
 picture_QR_code = tk.Canvas(main_windows, width=100, height=100, bg="red")
 #message_QR_code = tk.Label(main_windows, text="t", bg="black", fg="white")
 
-button_loading = tk.Button(main_windows, text="Charger", command = lambda : charger())
-button_decode = tk.Button(main_windows, text="Décoder", command = lambda : decode("frame.png"))
+button_loading = tk.Button(main_windows, text="Charger", command = lambda : selection_fichier())
+button_decode = tk.Button(main_windows, text="Décoder", command = lambda : decode())
 
 picture_QR_code.grid(column=1, row=0)
 #message_QR_code.grid(column=1, row=1)
